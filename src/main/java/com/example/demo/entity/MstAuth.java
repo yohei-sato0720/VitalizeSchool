a42e948ea74e1dfbc587cae24962689d76853e01
大川泰平 <okawataihei@ookawataiheinoMacBook-puro.local>
2020-05-20 14:00:23 +0900

    修正

---
+++ src/main/java/com/example/demo/entity/MstAuth.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+/**
+ * 権限マスタ情報 Entity
+ */
+@Entity
+@Data
+@Table(name="mst_auth")
+public class MstAuth implements Serializable {
+    /**
+     * 権限ID
+     */
+    @Id
+    @Column(name="auth_id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long auth_id;
+    /**
+     * 管理権限ステータス
+     */
+    @Column(name="status")
+    private Integer status;
+    /**
+     * 管理権限名
+     */
+    @Column(name="status_name")
+    private String statusName;
+    /**
+     * 機能ステータス
+     */
+    @Column(name="function_status")
+    private Integer functionStatus;
+    /**
+     * 更新日時
+     */
+    @Column(name="update_date")
+    private Date updateDate;
+    /**
+     * 登録日時
+     */
+    @Column(name="create_date")
+    private Date createDate;
+    /**
+     * 削除日時
+     */
+    @Column(name="delete_date")
+    private Date deleteDate;
+}
--- src/main/java/com/example/demo/repositories/MstAuthRepository.java
+++ src/main/java/com/example/demo/repositories/MstAuthRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class MstAuthRepository {
+import com.example.demo.entity.MstAuth;
 
-}
+@Repository
+public interface MstAuthRepository extends JpaRepository<MstAuth, Long> {}
